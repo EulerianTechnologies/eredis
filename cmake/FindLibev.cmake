513d690348b70ae92713bb3505f41a6169890580
Karl Lenz <xorangekiller@gmail.com>
2018-01-28 19:35:19 -0500

    Support out-of-tree builds
    This is a fairly major rework of the Eredis CMake build scripts to
    support out-of-tree builds, which is strongly encouraged by the CMake
    developers, and makes simultaneously building for multiple platforms
    much easier. While I was at it, I changed a few other things to try to
    make the Eredis build conform to best practices. I tried my best not to
    unnecessarily change anything, and to adhere to the existing style in
    the Eredis CMake build scripts as best I could.
    
    1. Rather than just using add_definitions() to add all compiler flags,
       we now have the release and debug flags properly split out. To build
       with fully debug symbols (which was hard-coded before), pass the flag
       `-DCMAKE_BUILD_TYPE=Debug` to CMake when configuring Eredis.
    
    2. The documentation is now installed to /usr/local/share/doc/eredis/html
       by default (if we are using CMake 3.0 or later, anyway), and there are
       new flags to control it. `SKIP_DOCUMENTATION` will disable the `doc`
       target unconditionally, regardless of whether or not Doxygen is
       installed on the system, and `FORCE_INSTALL_DOCUMENTATION` will force
       the documentation to be installed regardless of the CMake version.
    
    3. I added a Git submodule for src/hiredis and, optionally, allowed an
       alternate path to be specified for it with the `HIREDIS_SOURCE_DIR`
       flag. The legacy behavior of cloning the hiredis repo from GitHub is
       still there too as a fallback path, but I would strongly recommend
       using one of the other two methods instead. Cloning an arbitrary Git
       repo inside of an existing Git repo (without using submodules) is
       usually a bad idea, and newer versions of Git warn about it.
    
    4. CMake now searches for libev (Eredis' only real build dependency -
       hiredis doesn't really count since parts of it are technically built
       as part of the Eredis build) like any other library. Not only does
       that mean that we get much nicer error messages if it is not
       installed (during the configure step, not during the build step like
       before), but it gives the user more control over where and how libev
       is discovered. This is particularly important for cross-compiled
       builds of Eredis.

---
+++ cmake/FindLibev.cmake
@@ -0,0 +1,88 @@
+# Copyright (c) 2018 by Karl Lenz <xorangekiller@gmail.com>
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or
+# without modification, are permitted provided that the following
+# conditions are met:
+#
+# * Redistributions of source code must retain the above copyright
+#   notice, this list of conditions and the following disclaimer.
+#
+# * Redistributions in binary form must reproduce the above
+#   copyright notice, this list of conditions and the following
+#   disclaimer in the documentation and/or other materials
+#   provided with the distribution.
+#
+# * Neither the name of Eulerian Technologies nor the names of its
+#   contributors may be used to endorse or promote products
+#   derived from this software without specific prior written
+#   permission.
+#
+# THIS SOFTWARE IS PROVIDED BY <COPYRIGHT HOLDER> ``AS IS'' AND
+# ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED
+# TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+# A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL
+# <COPYRIGHT HOLDER> OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+# INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
+# BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+# LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+# (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF
+# THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+# SUCH DAMAGE.
+#
+#.rst:
+# FindLibev
+# ---------
+#
+# Locate libev, the high-performance event loop library that is loosely modeled
+# after libevent, if it is installed on the system.
+#
+# libev homepage: http://software.schmorp.de/pkg/libev.html
+#
+# Defines the following variables:
+#
+# ::
+#
+#    LIBEV_FOUND       - System has libev (include and library directories found)
+#    LIBEV_INCLUDE_DIR - The libev include directories
+#    LIBEV_LIBRARY     - The libev library for linking
+#
+#
+#
+# Accepts the following variables as input:
+#
+# ::
+#
+#    LIBEV_ROOT_DIR   - Set this variable to the root installation of libev if
+#                       the module has problems finding the proper installation
+#                       path. (This may be useful when cross-compiling, but is
+#                       generally unnecessary otherwise.)
+#
+
+FIND_PATH( LIBEV_ROOT_DIR
+    NAMES include/ev.h
+)
+
+FIND_PATH( LIBEV_INCLUDE_DIR
+    NAMES ev.h
+    HINTS ${LIBEV_ROOT_DIR}/include
+)
+
+FIND_LIBRARY( LIBEV_LIBRARY
+    NAMES ev
+    HINTS ${LIBEV_ROOT_DIR}/lib
+)
+
+INCLUDE( FindPackageHandleStandardArgs )
+FIND_PACKAGE_HANDLE_STANDARD_ARGS( libev DEFAULT_MSG
+    LIBEV_INCLUDE_DIR
+    LIBEV_LIBRARY
+)
+
+MARK_AS_ADVANCED(
+    LIBEV_ROOT_DIR
+    LIBEV_INCLUDE_DIR
+    LIBEV_LIBRARY
+)
